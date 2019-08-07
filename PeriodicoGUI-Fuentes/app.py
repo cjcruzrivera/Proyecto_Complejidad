ba396c46a632498976c3ec723be1955b6cfe3c8d
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2019-08-07 18:43:34 -0500

    toma de datos con validaciones

--- PeriodicoGUI-Fuentes/app.py
+++ PeriodicoGUI-Fuentes/app.py
@@ -1,15 +1,26 @@
-from flask import Flask, abort, request, render_template
+from flask import Flask, abort, request, render_template, redirect
 app = Flask(__name__)
 
 
-@app.route('/',methods=['GET'])
+@app.route('/',methods=['POST','GET'])
 def index():
-    return render_template("index.html")
-
-@app.route('/resultado',methods=['POST','GET'])
-def resultado():
-    return render_template("resultado.html")
-
+    if request.method == "POST":
+        resultado = "AQUI IRA EL RESULTADO"
+        temas = request.form.getlist('tema')
+        temasA = []
+        for tema in temas:
+            if tema == "":
+                continue
+            temaA = {}
+            temaA["tema"] = tema
+            temaA["min"] = request.form.get("min"+tema,"0")
+            temaA["max"] = request.form.get("max"+tema,"0")
+            temaA["potencial"] = request.form.get("potencial"+tema,"0")
+            temasA.append(temaA)
+        print(temasA)
+        return render_template("resultado.html", resultado=resultado)
+    elif request.method == "GET":
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
--- PeriodicoGUI-Fuentes/static/js/index.js
+++ PeriodicoGUI-Fuentes/static/js/index.js
@@ -6,9 +6,12 @@
         // Loop over them and prevent submission
         var validation = Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
+                var validity = form.checkValidity();
+                console.log(validity)
+                if (validity === false) {
                     event.preventDefault();
                     event.stopPropagation();
+
                 }
                 form.classList.add('was-validated');
             }, false);
@@ -26,13 +29,14 @@ $(document).ready(function () {
     $("#newTopic").click(function () {
         $("#calcular").prop("disabled", true);
         $(this).prop("disabled", true);
+        $(".removeTopic").prop("disabled", true);
         var newRow = $("#example").clone().appendTo('tbody');
         newRow.removeClass("d-none");
         newRow.find(".newTopicLabel").attr("id", "newTopicValue");
-        
 
-        
-        
+
+
+
         $(".addTopic").on("click", function () {
             var element = $("#newTopicValue");
             var valTopic = element.val();
@@ -41,24 +45,59 @@ $(document).ready(function () {
                 element.addClass("is-invalid");
                 element.next(".invalid-feedback").show()
                 return;
-            }else{
+            } else {
                 element.next(".invalid-feedback").hide();
-                console.log(element.parent().parent().attr("id"));
-                element.parent().parent().find("#max").attr("name", "max"+valTopic).attr("id", "max"+valTopic);
-                element.parent().parent().find("#min").attr("name", "min"+valTopic).attr("id", "min"+valTopic);
-                element.parent().parent().find("#potencial").attr("name", "potencial"+valTopic).attr("id", "potencial"+valTopic);
-                element.parent().text(valTopic);
-                element.remove();
+                element.parent().parent().find("#max").attr("name", "max" + valTopic).attr("id", "max" + valTopic).attr("required", "true");
+                element.parent().parent().find("#min").attr("name", "min" + valTopic).attr("id", "min" + valTopic).attr("required", "true");
+                element.parent().parent().find("#potencial").attr("name", "potencial" + valTopic).attr("id", "potencial" + valTopic).attr("required", "true");;
+                element.parent().parent().removeAttr("id");
+                element.parent().append(valTopic);
+
+                element.removeClass("newTopicLabel");
+                element.addClass("d-none");
                 $(this).removeClass("addTopic btn-success").addClass("removeTopic btn-warning").text("-");
-                $(".removeTopic").on("click",function () {
+                $(".removeTopic").on("click", function () {
                     $(this).parent().parent().remove();
                 });
                 $("#calcular").prop("disabled", false);
+                $(".removeTopic").prop("disabled", false);
                 $("#newTopic").prop("disabled", false);
             }
 
         })
     });
 
-})
+    $("input[name^='max']").on("change", function () {
+        var divInvalid = $(this).next(".invalid-feedback"); 
+        divInvalid.hide();
+        $(this).removeClass("is-invalid");
+        var name = $(this).attr("name");
+        var topicName = name.split("max")[1];
+        var minVal = $("#min"+topicName).val();
+        var maxVal = $(this).val();
+        if(minVal == "" || maxVal == ""){
+            return;
+        }
+        if(minVal > maxVal){
+            divInvalid.show();
+            $(this).addClass("is-invalid").val("");
+        }
+    })
 
+    $("input[name^='min']").on("change", function () {
+        var divInvalid = $(this).next(".invalid-feedback"); 
+        divInvalid.hide();
+        $(this).removeClass("is-invalid");
+        var name = $(this).attr("name");
+        var topicName = name.split("min")[1];
+        var maxVal = $("#max"+topicName).val();
+        var minVal = $(this).val();
+        if(minVal == "" || maxVal == ""){
+            return;
+        }
+        if(minVal > maxVal){
+            divInvalid.show();
+            $(this).addClass("is-invalid").val("");
+        }
+    })
+})
