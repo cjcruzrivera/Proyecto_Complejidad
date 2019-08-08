a9bdcd6b9b52d080a025765f832d9de717c9c2eb
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2019-08-08 11:08:14 -0500

    ajuste a lectura de archivo

--- PeriodicoGUI-Fuentes/app.py
+++ PeriodicoGUI-Fuentes/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, abort, request, render_template, redirect
-import os 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+from processing import processFile
 app = Flask(__name__)
 
 
@@ -8,50 +7,7 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         resultado = ""
-        temas = request.form.getlist('tema')
-        temasA = []
-        minA = []
-        maxA = []
-        potencialA = []
-
-        capacidad = request.form.get("capacidad", 10)
-        for tema in temas:
-            if tema == "":
-                continue
-            minVal = request.form.get("min"+tema,"0")
-            maxVal = request.form.get("max"+tema,"0")
-            potencialVal = request.form.get("potencial"+tema,"0")
-            temasA.append(tema)
-            minA.append(minVal)
-            maxA.append(maxVal)
-            potencialA.append(potencialVal)
-
-        file = open("Modelos/Periodico/PeriodicoDatos.dzn", "w")
-        cantidad = len(temasA)
-        file.write("n = "+ str(cantidad) + " \n")
-        file.write("P = "+ capacidad + " \n")
-        
-        textMin = "minPag = ["
-        for minP in minA:
-            textMin += minP+", "
-        textMin = textMin.strip(", ")
-        textMin += "]\n"
-        file.write(textMin)
-
-        textMax = "maxPag = ["
-        for maxP in maxA:
-            textMax += maxP+", "
-        textMax = textMax.strip(", ")
-        textMax += "]\n"
-        file.write(textMax)
-
-        textPot = "potLect = ["
-        for potP in potencialA:
-            textPot += potP+", "
-        textPot = textPot.strip(", ")
-        textPot += "]"
-        file.write(textPot)
-        file.close()
+        processFile(request)
         return render_template("resultado.html", resultado=resultado)
     elif request.method == "GET":
         return render_template("index.html")
---
+++ PeriodicoGUI-Fuentes/processing.py
@@ -0,0 +1,48 @@
+import os
+
+
+def processFile(request):
+    temas = request.form.getlist('tema')
+    temasA = []
+    minA = []
+    maxA = []
+    potencialA = []
+
+    capacidad = request.form.get("capacidad", 10)
+    for tema in temas:
+        if tema == "":
+            continue
+        minVal = request.form.get("min"+tema, "0")
+        maxVal = request.form.get("max"+tema, "0")
+        potencialVal = request.form.get("potencial"+tema, "0")
+        temasA.append(tema)
+        minA.append(minVal)
+        maxA.append(maxVal)
+        potencialA.append(potencialVal)
+
+    file = open("Modelos/Periodico/PeriodicoDatos.dzn", "w")
+    cantidad = len(temasA)
+    file.write("n = " + str(cantidad) + " \n")
+    file.write("P = " + capacidad + " \n")
+
+    textMin = "minP = ["
+    for minP in minA:
+        textMin += minP+", "
+    textMin = textMin.strip(", ")
+    textMin += "]\n"
+    file.write(textMin)
+
+    textMax = "maxP = ["
+    for maxP in maxA:
+        textMax += maxP+", "
+    textMax = textMax.strip(", ")
+    textMax += "]\n"
+    file.write(textMax)
+
+    textPot = "readers = ["
+    for potP in potencialA:
+        textPot += potP+", "
+    textPot = textPot.strip(", ")
+    textPot += "]"
+    file.write(textPot)
+    file.close()
